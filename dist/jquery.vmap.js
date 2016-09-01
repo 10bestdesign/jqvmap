454e9fa3d1ef15a90f84ff63775ad5c14940f857
Egor Komarov <EKomarov@lanit.ru>
2016-09-01 17:04:03 +0300

    revert ./dist changes

--- dist/jquery.vmap.js
+++ dist/jquery.vmap.js
@@ -4,7 +4,7 @@
  * @version 1.5.1
  * @link http://jqvmap.com
  * @license https://github.com/manifestinteractive/jqvmap/blob/master/LICENSE
- * @builddate 2016/09/01
+ * @builddate 2016/06/02
  */
 
 var VectorCanvas = function (width, height, params) {
@@ -73,6 +73,7 @@ var JQVMap = function (params) {
   params = params || {};
   var map = this;
   var mapData = JQVMap.maps[params.map];
+  var mapPins;
 
   if( !mapData){
     throw new Error('Invalid "' + params.map + '" map parameter. Please make sure you have loaded this map file in your HTML.');
@@ -112,7 +113,11 @@ var JQVMap = function (params) {
       var resizeEvent = jQuery.Event('resize.jqvmap');
       jQuery(params.container).trigger(resizeEvent, [newWidth, newHeight]);
 
-      map.positionPins();
+      if(mapPins){
+        jQuery('.jqvmap-pin').remove();
+        map.pinHandlers = false;
+        map.placePins(mapPins.pins, mapPins.mode);
+      }
     }
   });
 
@@ -255,6 +260,11 @@ var JQVMap = function (params) {
   this.bindZoomButtons();
 
   if(params.pins) {
+    mapPins = {
+      pins: params.pins,
+      mode: params.pinMode
+    };
+
     this.pinHandlers = false;
     this.placePins(params.pins, params.pinMode);
   }
@@ -270,6 +280,11 @@ var JQVMap = function (params) {
         }
       }
     }
+
+    mapPins = {
+      pins: pins,
+      mode: 'content'
+    };
 
     this.placePins(pins, 'content');
   }
@@ -838,13 +853,17 @@ JQVMap.prototype.positionPins = function(){
     var scale = map.scale;
     var rootCoords = map.canvas.rootGroup.getBoundingClientRect();
     var mapCoords = map.container[0].getBoundingClientRect();
+    var coords = {
+      left: rootCoords.left - mapCoords.left,
+      top: rootCoords.top - mapCoords.top
+    };
 
     var middleX = (bbox.x * scale) + ((bbox.width * scale) / 2);
     var middleY = (bbox.y * scale) + ((bbox.height * scale) / 2);
 
     pinObj.css({
-      left: (rootCoords.left - mapCoords.left) + middleX - (pinObj.width() / 2),
-      top: (rootCoords.top - mapCoords.top) + middleY - (pinObj.height() / 2)
+      left: coords.left + middleX - (pinObj.width() / 2),
+      top: coords.top + middleY - (pinObj.height() / 2)
     });
   });
 };
